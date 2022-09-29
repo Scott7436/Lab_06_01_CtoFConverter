4bf58800acfa7e10a291cde3f72045e6a0180c3f
Scott7436 <gogginse@mail.uc.edu>
2022-09-29 13:14:47 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String trash = "";
+        double tempC;
+        double tempF;
+
+
+        System.out.println("Please enter the temperature in Celcius: ");
+
+        if(in.hasNextDouble())
+        {
+            tempC = in.nextDouble();
+            in.nextLine();
+            tempF = tempC * 1.8 + 32;
+            System.out.println("You entered " + tempC +" which is " + tempF +  " in Fahrenheit");
+
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("\nYou said your temperature was: " + trash);
+            System.out.println("Run the program again and enter a valid amount!");
+            System.exit(0);
+
+        }
+
+    }
+}
